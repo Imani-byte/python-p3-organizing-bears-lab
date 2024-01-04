816af0dfcfe680653ecbde5f9cc544761510e8af
naisiae.kasura@gmail.com <imani.kasura@student.moringaschool.com>
2024-01-04 23:30:30 +0300

    complete task

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+  ('Tabitha', 4, 'F', 'Gray', 'Calm', 1),
+  ('Sergeant Brown', 6, 'M', 'Brown', 'Disciplined', 1),
+  ('Melissa', 2, 'F', 'White', 'Energetic', 1),
+  ('Grinch', 8, 'M', 'Green', 'Grumpy', 1),
+  ('Wendy', 7, 'F', 'Brown', 'Gentle', 1),
+  (NULL, NULL, NULL, NULL, NULL, NULL);
